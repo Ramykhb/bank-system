10706f7f41227665f844daca9c48b95851f35932
Ramy Khachab <ramykhb18@gmail.com>
2024-11-11 13:13:09 +0200

    Initial Commit

---
+++ main.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+using namespace std;
+
+struct transaction {
+    string date;
+    double amount;
+    transaction *next;
+};
+
+struct account {
+    string IBAN;
+    string accountName;
+    double balance;
+    string currency;
+    double limitDepositPerDay;
+    double limitWithdrawPerMonth;
+    transaction *txn;
+    account *next;
+};
+
+struct user {
+    int userID;
+    string fname;
+    string lname;
+    account *acct; 
+    user *next, *previous; 
+};
+
+struct userList {
+    user *head, *tail;
+};
+
+void input_data()
+{
+    
+}
+
+void transfer(userList *mainlist, double amount, account *sender, account *target)
+{
+    // Ma 3refet sewe shi
+}
+
+int main ()
+{
+    input_data();
+}
