9fc8bf030aad8b440aba51bc244580aa4e20ae98
Ramy Khachab <ramykhb18@gmail.com>
2024-11-12 21:29:32 +0200

    Added new 'add_account' function (not tested)

--- main.cpp
+++ main.cpp
@@ -46,6 +46,46 @@ void transfer(userList *mainlist, double amount, account *sender, account *targe
     // hahahaha
 }
 
+void add_account(userList *mainlist, user *client, account *newacc)
+{
+
+    // NOTE: ma baaref iza zabta ma sawet testing, baaden mensewe l testing...
+
+    user *usercur = mainlist->head;
+    account *acccur;
+    while (usercur != NULL)
+    {
+        if (usercur == client)
+        {
+            acccur = usercur->acct;
+            while (acccur->next != NULL)
+            {
+                if (acccur->IBAN == newacc->IBAN)
+                {
+                    cout << "Account already exists..." << endl;
+                    return;
+                }
+                acccur = acccur->next;
+            }
+
+            if (acccur->IBAN == newacc->IBAN)
+            {
+                cout << "Account already exists..." << endl;
+                return;
+            }
+
+            acccur->next = newacc;
+            cout << "Account added successfully..." << endl;
+            return;
+        }
+
+        usercur = usercur->next;
+    }
+
+    cout << "User not found..." << endl;
+    return;
+}
+
 int main()
 {
     input_data();
