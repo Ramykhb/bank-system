d324ae42f8371fa4312f1f5ad445810ebafb9ce1
Ramy Khachab <ramykhb18@gmail.com>
2024-11-15 11:58:08 +0200

    Added delete_transactions fucntion

--- main.cpp
+++ main.cpp
@@ -1,6 +1,9 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <chrono>
+#include <sstream>
+#include <iomanip>
 using namespace std;
 
 struct transaction
@@ -35,6 +38,39 @@ struct userList
 {
     user *head, *tail;
 };
+
+bool isValidDate(const string &dateStr)
+{
+    tm time = {};
+    istringstream ss(dateStr);
+    ss >> get_time(&time, "%d/%m/%Y");
+    if (ss.fail())
+    {
+        cout << "Invalid date format...\n";
+        return false;
+    }
+
+    auto parsedTime = chrono::system_clock::from_time_t(mktime(&time));
+    auto now = chrono::system_clock::now();
+    if (parsedTime > now)
+        return true;
+    cout << "Enter an upcoming date...\n";
+    return false;
+}
+
+bool compareDate(const string &dateStr, const string &dateStr1)
+{
+    tm time1 = {}, time2 = {};
+    istringstream ss1(dateStr), ss2(dateStr1);
+    ss1 >> get_time(&time1, "%d/%m/%Y");
+    ss2 >> get_time(&time2, "%d/%m/%Y");
+
+    auto tp1 = chrono::system_clock::from_time_t(mktime(&time1));
+    auto tp2 = chrono::system_clock::from_time_t(mktime(&time2));
+    if (tp1 <= tp2)
+        return true;
+    return false;
+}
 
 void input_data()
 {
@@ -84,6 +120,46 @@ void add_account(userList *mainlist, user *client, account *newacc)
 
     cout << "User not found..." << endl;
     return;
+}
+
+void delete_transactions(userList *mainlist, string date)
+{
+    user *usercur = mainlist->head;
+    account *acccur;
+    transaction *transcur;
+    transaction *previous;
+
+    while (usercur != NULL)
+    {
+        acccur = usercur->acct;
+        while (acccur != NULL)
+        {
+            if (acccur->txn != NULL)
+            {
+                transcur = acccur->txn->next;
+                while (transcur != NULL)
+                {
+                    if (compareDate(transcur->date, date))
+                    {
+                        previous->next = transcur->next;
+                        delete transcur;
+                        transcur = previous;
+                    }
+                    previous = transcur;
+                    transcur = transcur->next;
+                }
+                transcur = acccur->txn;
+                if (compareDate(transcur->date, date))
+                {
+                    acccur->txn->next = transcur->next;
+                    delete transcur;
+                }
+                acccur = acccur->next;
+            }
+        }
+
+        usercur = usercur->next;
+    }
 }
 
 void export_data(userList *mainlist)
